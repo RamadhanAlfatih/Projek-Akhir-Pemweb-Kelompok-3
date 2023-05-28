4675a6a5ec6192911ee516d95cd144419562338e
RamadhanAlfatih <ramadhanalfatih10@gmail.com>
2023-05-29 01:41:41 +0700

    struktur write-review

--- upload.php
+++ upload.php
@@ -1,13 +1,21 @@
 <?php
-$target_path = "images/";
-$target_foto = $target_path . basename($_FILES["foto"]["name"]);
-$imgType = strtolower(pathinfo($target_foto, PATHINFO_EXTENSION));
-if ($imgType != "jpg" && $imgType != "png") {
-    echo "Maaf, cuma bisa foto ekstensi .jpg atau .png saja";
-    exit();
+if(isset($_FILES['image'])) {
+  $file_name = $_FILES['image']['name'];
+  $file_size = $_FILES['image']['size'];
+  $file_tmp = $_FILES['image']['tmp_name'];
+  $file_type = $_FILES['image']['type'];
+  
+  // Lokasi penyimpanan file yang diunggah
+  $upload_path = 'images/';
+
+  // Membuat direktori jika belum ada
+  if(!is_dir($upload_path)){
+    mkdir($upload_path, 0755, true);
+  }
+
+  // Pindahkan file yang diunggah ke direktori tujuan
+  move_uploaded_file($file_tmp, $upload_path . $file_name);
+
+  echo "File berhasil diunggah: " . $upload_path . $file_name;
 }
-if (move_uploaded_file($_FILES["foto"]["tmp_name"], $target_foto)) {
-    echo "Foto " . htmlspecialchars(basename($_FILES["foto"]["name"])) . " sukses ter-uploaad";
-} else {
-    echo "Maaf, sesuatu ada yang salah saat mengupload foto.";
-}
+?>
--- write-review.php
+++ write-review.php
@@ -21,38 +21,93 @@
             </div>
         </div>
         <div class="container2">
-            <div class="photo-container">
+            <div class="form-container">
                 <form action="upload.php" method="post" enctype="multipart/form-data">
-                    <p>Choose a photo to upload:</p>
-                    <input type="file" id="foto" name="foto" accept=".jpg,.png" onchange="lihatFoto()">
-                    <br>
-                    <br>
-                    <img src="#" alt="Hasil upload" id="lihat" width="200" height="200" style="display:none;">
-                    <br>
-                    <br>
-                    <input type="submit" value="upload" name="submit">
+                    <div id="image-dropzone" onclick="document.getElementById('image-upload').click()">
+                        <div class="dropzone-text">Drag and drop an image here or click to select</div>
+                        <input type="file" id="image-upload" name="image" accept="image/*" onchange="previewImage(event)" />
+                        <img id="preview-image" src="#" alt="Preview" />
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="Upload" name="submit">
+                    </div>
                 </form>
-                <script>
-                    function readURL(input) {
-                        if (input.files && input.files[0]) {
-                            var reader = new FileReader()
-                            reader.onload = function(e) {
-                                document.getElementById('lihat').src = e.target.result
-                                document.getElementById('lihat').style.display = 'block'
-                            }
-                            reader.readAsDataURL(input.files[0])
-                        }
-                    }
-                    document.getElementById("foto").addEventListener("change", function() {
-                        readURL(this)
-                    })
-                </script>
             </div>
-            <div class="input-container">
-                <input type="text" name="restaurant-name" placeholder="Restaurant name" required>
+            <div class="form-container">
+                <form>
+                    <div class="form-group">
+                        <input type="text" name="deskripsi" placeholder="Description & Review" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="Submit" name="submit">
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+    <script>
+        const dropzone = document.getElementById('image-dropzone');
+        const input = document.getElementById('image-upload');
+        const preview = document.getElementById('preview-image');
+
+        dropzone.addEventListener('dragover', (e) => {
+            e.preventDefault();
+            dropzone.classList.add('dragover');
+        });
+
+        dropzone.addEventListener('dragleave', () => {
+            dropzone.classList.remove('dragover');
+        });
+
+        dropzone.addEventListener('drop', (e) => {
+            e.preventDefault();
+            dropzone.classList.remove('dragover');
+
+            const file = e.dataTransfer.files[0];
+            if (file) {
+                const reader = new FileReader();
+
+                reader.addEventListener('load', () => {
+                    preview.setAttribute('src', reader.result);
+                    preview.style.display = 'block';
+                });
+
+                reader.readAsDataURL(file);
+            }
+        });
+
+        input.addEventListener('change', () => {
+            const file = input.files[0];
+            if (file) {
+                const reader = new FileReader();
+
+                reader.addEventListener('load', () => {
+                    preview.setAttribute('src', reader.result);
+                    preview.style.display = 'block';
+                });
+
+                reader.readAsDataURL(file);
+            }
+        });
+
+        function previewImage(event) {
+            const file = event.target.files[0];
+            const preview = document.getElementById('preview-image');
+
+            if (file) {
+                const reader = new FileReader();
+
+                reader.addEventListener('load', () => {
+                    preview.setAttribute('src', reader.result);
+                    preview.style.display = 'block';
+                });
+
+                reader.readAsDataURL(file);
+            } else {
+                preview.style.display = 'none';
+            }
+        }
+    </script>
 </body>
 
 </html>
